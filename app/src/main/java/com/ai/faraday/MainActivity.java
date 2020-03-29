8ac5989bfa98bd481f13b17bd5515c85c2e4cc16
nik-1207 <nikhil.mohan_cs18@gla.ac.in>
2020-03-29 05:40:30 -0700

    setting Screen layout
    Signed-off-by: nik-1207 <nikhil.mohan_cs18@gla.ac.in>

--- app/src/main/java/com/ai/faraday/MainActivity.java
+++ app/src/main/java/com/ai/faraday/MainActivity.java
@@ -1,18 +1,33 @@
 package com.ai.faraday;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.speech.tts.Voice;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    Button voice;
+    TextView display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        voice= findViewById(R.id.button);
+        display=findViewById(R.id.textView1);
 
+        voice.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+               display.setText("Listening.....");
+
+            }
+        }
+        );
 
     }
 }
