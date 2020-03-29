7a1a72b855020baa968122302fe44bd5105f1e88
nik-1207 <nikhil.mohan_cs18@gla.ac.in>
2020-03-29 04:35:07 -0700

    setting Screen layout
    Signed-off-by: nik-1207 <nikhil.mohan_cs18@gla.ac.in>

--- app/src/main/java/com/ai/faraday/MainActivity.java
+++ app/src/main/java/com/ai/faraday/MainActivity.java
@@ -3,12 +3,16 @@ package com.ai.faraday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
